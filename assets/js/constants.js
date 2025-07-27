cc66a15561de397d724cfcf3a2aa3d3ac48ad050
shubrawat_deloitte <shubrawat@deloitte.com>
2025-07-28 01:08:33 +0530

    fix: animation

---
+++ assets/js/constants.js
@@ -0,0 +1,16 @@
+// constants.js
+// Centralized constants for hardcoded strings used in the site
+
+const SITE_CONSTANTS = {
+  siteName: "AccounTax Solution",
+  companyName: "AccounTax Solution",
+  address: "418, Industrial Area Phase 2, Chandigarh, India 160002",
+  phone: "+91 97805 18960",
+  email: "accountaxsolutions@gmail.com",
+  copyright: "© Copyright AccounTax Solution All Rights Reserved",
+  newsletterText: "Subscribe to our newsletter and receive the latest news about our products and services!",
+  // Add more as needed
+};
+
+// Example: Export for use in main.js or inline scripts
+// window.SITE_CONSTANTS = SITE_CONSTANTS;
